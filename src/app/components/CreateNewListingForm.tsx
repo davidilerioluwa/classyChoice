e586fb497dbbfc86c2d08eb9a1b863e1bfcb889d
David Godswill <davidilerioluwa1998@gmail.com>
2025-01-26 00:12:50 +0100

    added the variations feature

--- src/app/components/CreateNewListingForm.tsx
+++ src/app/components/CreateNewListingForm.tsx
@@ -334,7 +334,7 @@ const CreateNewListingForm = ({setShowListingForm,EditListingId,setEditListingId
               <h1 className='font-bold'>Variations</h1>
               <div className='p-2 flex flex-col gap-2'>
                 {variations.map((variation)=>(
-                <div className='flex flex-col gap-1'>
+                <div key={variation.variation} className='flex flex-col gap-1'>
                   {oldVariationValue?
                   <div className='flex w-full justify-between w-full gap-4'>
                     <input value={newVariationValue} onChange={(e)=>setNewVariationValue(e.target.value)} className='px-2 py-2 text-purple-800 outline outline-[1px] outline-purple-800 rounded-md w-full'/>
