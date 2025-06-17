89e17d7e15340121ede027c4fe19df05379a2f44
David Godswill <davidilerioluwa1998@gmail.com>
2025-06-17 11:13:32 +0100

    added a contact form

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,15 +15,15 @@ return (
       </div>
       <div className=' p-8 py-24 md:p-20 z-10 hidden md:flex flex-col col-span-7 gap-2 md:gap-4 justify-center items-start text-white'>
           <div className='absolute w-1/4 left-0 rounded-md   bg-purple-900 z-[-1] h-72 md:h-1/2'></div>
-          <p>Shop Smart, Live Better</p>
-          <p className='text-2xl md:text-4xl lg:text-5xl font-bold text-white'> Discover Endless Deals And Unbeatable Quality-Everything You Need.</p>
+          <p className='text-2xl md:text-4xl lg:text-6xl font-bold text-white'>Shop Smart, Live Better</p>
+          <p > Discover Endless Deals And Unbeatable Quality-Everything You Need.</p>
           <Link href={"/search"} className=' text-purple-900 bg-white  px-4 py-2 rounded-sm text-lg font-bold'>SHOP NOW</Link>
       </div>
       <div className='absolute top-32 bg-purple-800/70 backdrop-blur-md mx-8 md:hidden rounded-lg shadow-lg drop-shadow-md border border-white/20 p-8 py-18 z-10 flex flex-col col-span-7 gap-2 md:gap-4 justify-center items-start text-white'>
           <div className='absolute md:w-1/4 left-0 rounded-md  md:bg-purple-900 z-[-1] h-72 md:h-1/2'></div>
-          <p>Shop Smart, Live Better</p>
-          <p className='text-2xl md:text-4xl font-bold text-white'> Discover Endless Deals And Unbeatable Quality-Everything You Need.</p>
-          <Link href={"/search"} className=' text-purple-900 bg-white px-6 py-2 rounded-sm font-bold text-lg'>SHOP NOW</Link>
+          <p className='text-2xl md:text-4xl font-bold text-white'>Shop Smart, Live Better</p>
+          <p className=''> Discover Endless Deals And Unbeatable Quality-Everything You Need.</p>
+          <Link href={"/search"} className=' text-purple-900 bg-white px-6 py-2 rounded-md font-bold text-lg'>SHOP NOW</Link>
       </div>
       <img src="clothes.jpeg" className='object-cover w-full h-full hidden md:flex md:h-screen col-span-5' alt="" />
     </section>
