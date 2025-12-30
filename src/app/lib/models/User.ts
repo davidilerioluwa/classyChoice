4f0774723be1f7cbce27f41dd7db307cf7079cd6
LAVOISIER <davidilerioluwa1998@gmail.com>
2025-12-30 13:50:11 +0100

    added sign in with email

--- src/app/lib/models/User.ts
+++ src/app/lib/models/User.ts
@@ -1,47 +1,50 @@
-import mongoose,{Schema, Document } from "mongoose";
+import mongoose, { Schema, Document } from "mongoose";
 
 export interface iUser extends Document {
-    email: string,
-    name: string,
-    phoneNumber:string,
-    address: string,
-    city: string,
-    state:string,
-    accountType:string
+  email: string;
+  name: string;
+  phoneNumber: string;
+  address: string;
+  city: string;
+  state: string;
+  accountType: string;
 }
 
-const userSchema: Schema= new mongoose.Schema({
-    email :{
-        type: String,
-        required: true
-    },
-    name :{
-        type: String,
-        required: true
-    },
-    phoneNumber:{
-        type:String,
-        required:false
-    },
-    address:{
-        type: String,
-        required: false
-    },
-    city: {
-        type: String,
-        required: false
-    },
-    state: {
-        type: String,
-        required: false,
-    },
-    accountType:{
-        type: String,
-        required: false
-    }
+const userSchema: Schema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: true,
+  },
+  name: {
+    type: String,
+    required: true,
+  },
+  phoneNumber: {
+    type: String,
+    required: false,
+  },
+  address: {
+    type: String,
+    required: false,
+  },
+  city: {
+    type: String,
+    required: false,
+  },
+  state: {
+    type: String,
+    required: false,
+  },
+  accountType: {
+    type: String,
+    required: false,
+  },
+  provider: {
+    type: String,
+    required: false,
+  },
+});
 
-})
+const User = mongoose.models.User || mongoose.model<iUser>("User", userSchema);
 
-const User =mongoose.models.User || mongoose.model<iUser>("User",userSchema)
-
-export default User
+export default User;
