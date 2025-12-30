95403aa2261d5d8565e9df0ca4a4e36d50331e0a
LAVOISIER <davidilerioluwa1998@gmail.com>
2025-12-30 14:55:29 +0100

    added sign in with email

--- src/app/lib/models/Orders.ts
+++ src/app/lib/models/Orders.ts
@@ -1,65 +1,64 @@
-import mongoose,{Schema, Document } from "mongoose";
+import mongoose, { Schema, Document, Types } from "mongoose";
 export interface product {
-    productId: string,
-    quantity: string,
-    title:string,
-    price: number
+  productId: string;
+  quantity: string;
+  title: string;
+  price: number;
 }
 export interface iOrder extends Document {
-    items: Array<product>
-    time?: Date,
-    userId?:string
-    _id: string,
-    status:string,
-    amount: number,
-    orderId:string,
-    note:string,
-    paymentProof: {url:string,assetId:string},
-    alternativeAddress:string
+  items: Array<product>;
+  time?: Date;
+  userId?: string;
+  _id: Types.ObjectId;
+  status: string;
+  amount: number;
+  orderId: string;
+  note: string;
+  paymentProof: { url: string; assetId: string };
+  alternativeAddress: string;
 }
 
-const orderSchema: Schema= new mongoose.Schema({
-    items :{
-        type: Array<product>,
-        required: true
-    },
-    time:{
-        type:Date,
-        required:true
-    },
-    userId:{
-        type: String,
-        required: true
-    },
-    status:{
-        type:String,
-        required:true
-    },
-    amount:{
-        type:Number,
-        required:true
-    },
-    orderId:{
-        type: String,
-        required: true
-    },
-    
-    note:{
-        type:String,
-        required:false
-    },
-    paymentProof:{
-        type: {url:String,assetId:String},
-        required:true
-    },
-    alternativeAddress:{
-        type:String,
-        required:false
+const orderSchema: Schema = new mongoose.Schema({
+  items: {
+    type: Array<product>,
+    required: true,
+  },
+  time: {
+    type: Date,
+    required: true,
+  },
+  userId: {
+    type: String,
+    required: true,
+  },
+  status: {
+    type: String,
+    required: true,
+  },
+  amount: {
+    type: Number,
+    required: true,
+  },
+  orderId: {
+    type: String,
+    required: true,
+  },
 
-    }
+  note: {
+    type: String,
+    required: false,
+  },
+  paymentProof: {
+    type: { url: String, assetId: String },
+    required: true,
+  },
+  alternativeAddress: {
+    type: String,
+    required: false,
+  },
+});
 
-})
+const Order =
+  mongoose.models.order || mongoose.model<iOrder>("order", orderSchema);
 
-const Order =mongoose.models.order || mongoose.model<iOrder>("order",orderSchema)
-
-export default Order
+export default Order;
